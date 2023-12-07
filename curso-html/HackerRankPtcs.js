f8c38d1488833f860cc1bc260466c4826b23c027
Everth13 <everthreyes13@gmail.com>
2023-12-07 01:29:10 -0600

    7/12

---
+++ curso-html/HackerRankPtcs.js
@@ -0,0 +1,14 @@
+function elementos(){
+    let conjuntoA = [1,2,3,4,5,6];
+    let conjuntoB = [2,3,4,5,6,7,8];
+    let result = [];
+
+    for (let i= 0; i < 10; i++){
+        if (conjuntoA = conjuntoB){
+            result = result.unshift(conjuntoA[i]);
+            console.log("Ok");
+        }
+    }
+}
+
+elementos();
