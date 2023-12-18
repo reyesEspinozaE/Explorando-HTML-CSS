79cc2e2a5490c217298983b992930eb8559ed248
Everth13 <everthreyes13@gmail.com>
2023-12-18 01:51:42 -0600

    Update 18/12

--- curso-html/HackerRankPtcs.js
+++ curso-html/HackerRankPtcs.js
@@ -2,13 +2,17 @@ function elementos(){
     let conjuntoA = [1,2,3,4,5,6];
     let conjuntoB = [2,3,4,5,6,7,8];
     let result = [];
+    let result2 = 0;
 
-    for (let i= 0; i < 10; i++){
-        if (conjuntoA = conjuntoB){
-            result = result.unshift(conjuntoA[i]);
-            console.log("Ok");
+    result = conjuntoA;
+    for (let i= 0; i < 7; i++){
+        if (result[i +1] != conjuntoB[i]){
+            result.push(conjuntoB[i]);   
+            result2 = result.length;
         }
     }
+    console.log(result2);
 }
-
 elementos();
+
+  
